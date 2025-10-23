05c90ea9c5ad2dc6fb57a64d62522dec17d2e9e0
Haikal Risnandar <haikalrisnandar82@gmail.com>
2025-10-23 22:55:09 +0700

    create error handler

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,13 +6,16 @@ use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
         //
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+        // Handle error 404 (Not Found)
+        $exceptions->render(function (\Symfony\Component\HttpKernel\Exception\NotFoundHttpException $e, $request) {
+            return response()->view('errors.404', [], 404);
+        });
     })->create();
