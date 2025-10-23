24665432ca8876436773e832456c8d058a95d5d3
Haikal Risnandar <haikalrisnandar82@gmail.com>
2025-10-23 19:49:27 +0700

    init course

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use PHPUnit\Framework\Attributes\Group;
+
+Route::prefix('course')->group(function () {
+    Route::get('/');
+});
 
 Route::get('/', function () {
     return view('welcome');
