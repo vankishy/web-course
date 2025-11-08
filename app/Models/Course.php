66f430ef20fd7150b079062b10c9f5979478dbf2
vankishy <situmeangjuaratua15@gmail.com>
2025-11-08 18:48:07 +0700

    Add HasFactory trait to models

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -2,12 +2,14 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Course extends Model
 {
-    use SoftDeletes;
+    use HasFactory, SoftDeletes;
+    
     protected $table = 'course';
     protected $primaryKey = 'course_id';
     protected $fillable = [
