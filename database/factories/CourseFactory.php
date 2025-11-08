172a85abc3fc516bd5c3a1eab00e60aa7ec87a37
vankishy <situmeangjuaratua15@gmail.com>
2025-11-08 19:02:04 +0700

    Add Roadmap and Course factories

---
+++ database/factories/CourseFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Course>
+ */
+class CourseFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        // Menyediakan data palsu untuk kolom Anda
+        return [
+            'name' => fake()->sentence(4),
+            'desc' => fake()->paragraph(),
+            'image_path' => fake()->imageUrl(),
+        ];
+    }
+}
